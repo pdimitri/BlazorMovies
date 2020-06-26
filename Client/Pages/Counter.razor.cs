7b9fb09399f12991eac6f029d1ff089ceaf37858
Peter Dimitri <golem65@gmx.de>
2020-06-26 15:31:27 +0200

    Chapter 32 completed

--- Client/Pages/Counter.razor.cs
+++ Client/Pages/Counter.razor.cs
@@ -1,6 +1,7 @@
 ﻿using BlazorMovies.Client.Helpers;
 using BlazorMovies.Shared.Entities;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,14 +14,37 @@ namespace BlazorMovies.Client.Pages
         [Inject] SingletonService singleton { get; set; }
         [Inject] TransientService transient { get; set; }
         [Inject] IRepository repository { get; set; }
+        [Inject] IJSRuntime js { get; set; }
 
         private int currentCount = 0;
+        private static int currentCountStatic = 0;
 
-        private void IncrementCount()
+        [JSInvokable]
+        public async Task IncrementCount()
         {
             currentCount++;
+
+            //used in the services lesson
             transient.Value = currentCount;
             singleton.Value = currentCount;
+            
+            //used in lesson 31 Invoking C# Code from JS code
+            currentCountStatic++;
+            await js.InvokeVoidAsync("dotnetStaticInvocation");
+
+
+        }
+
+        private async Task IncrementCountJavascript()
+        {
+            await js.InvokeVoidAsync("dotnetInstanceInvocation", DotNetObjectReference.Create(this));
+        }
+
+        //This method may be called from within JS Code
+        [JSInvokable]
+        public static Task<int> GetCurrentCount()
+        {
+            return Task.FromResult(currentCountStatic);
         }
 
         private List<Movie> movies;
---
+++ Client/wwwroot/js/Utilities.js
@@ -0,0 +1,19 @@
+﻿
+//This is used to demonstrate the use of JS functions from C# Code. Counterpart can be found in Counter.razor.cs
+function my_function(message) {
+    console.log("from Utilities.js file in wwwroot " + message);
+}
+
+
+//This is used to demonstrate how to use C# Functions from JS code. Counterpart can be found in Counter.razor.cs
+function dotnetStaticInvocation() {
+    DotNet.invokeMethodAsync("BlazorMovies.Client", "GetCurrentCount")
+        .then(result => {
+            console.log("count from javascript " + result);
+        });
+}
+
+//From Lesson 32 Invoking Instance C# Methods from JS
+function dotnetInstanceInvocation(dotnetHelper) {
+    dotnetHelper.invokeMethodAsync("IncrementCount");
+}
